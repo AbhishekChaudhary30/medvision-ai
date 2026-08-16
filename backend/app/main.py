dbfd6ed6933ea41b8b1cf76bec32c3ae57388158
Abhishek Chaudhary <abhishekchaudhary8275@gmail.com>
2026-08-16 07:31:23 +0530

    Fix CORS for Vercel UI

--- backend/app/main.py
+++ backend/app/main.py
@@ -38,6 +38,7 @@ def create_app() -> FastAPI:
     application.add_middleware(
         CORSMiddleware,
         allow_origins=settings.cors_origins,
+        allow_origin_regex="https://.*",
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
